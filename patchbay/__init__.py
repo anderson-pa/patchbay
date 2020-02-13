b739774700418f60add87854ed829a5f08773863
Phillip Anderson <anderson.pa@gmail.com>
2020-02-13 18:40:15 -0500

    Remove package * imports. Rename package unit registry to ureg for consistency.

--- patchbay/__init__.py
+++ patchbay/__init__.py
@@ -2,10 +2,9 @@ import asyncio
 
 from pint import UnitRegistry, set_application_registry
 
-__all__ = ['hardware', 'qt']
-
 loop = asyncio.get_event_loop()
 
-unit = UnitRegistry()
-qty = unit.Quantity
-set_application_registry(unit)
+ureg = UnitRegistry()
+qty = ureg.Quantity
+set_application_registry(ureg)
+
