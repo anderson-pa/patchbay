519128e4ac6e2648de695a579052969350f49ad4
Phillip Anderson <anderson.pa@gmail.com>
2020-03-14 23:52:55 -0400

    Generalizing the subsystem infrastructure somewhat.

--- patchbay/hardware/scpi.py
+++ patchbay/hardware/scpi.py
@@ -1,15 +1,11 @@
 import builtins
-import json
-import weakref
-from collections import namedtuple
 from functools import lru_cache
 
 from patchbay import ureg
+from patchbay.hardware.subsystem import ValueConverter
 
-ScpiTypeConverter = namedtuple('ScpiTypeConverter', 'query, write')
 
-
-def parse_errors(err_str):
+def parse_error(err_str):
     """Split an error string into components.
 
     Assumed form for SCPI errors is `<int>, "description"`. This is used for
@@ -30,9 +26,9 @@ def scpi_error(arg=None):
     converters.
 
     :param arg: not used. only present for signature consistency.
-    :return: ScpiTypeConverter for errors
+    :return: ValueConverter for errors
     """
-    return ScpiTypeConverter(parse_errors, None)
+    return ValueConverter(parse_error, None)
 
 
 def scpi_bool(arg=None):
@@ -43,19 +39,18 @@ def scpi_bool(arg=None):
     boolean.
 
     :param arg: not used. only present for signature consistency
-    :return: ScpiTypeConverter for booleans
+    :return: ValueConverter for booleans
     """
-    return ScpiTypeConverter(lambda v: (bool(int(v))), int)
+    return ValueConverter(lambda v: (bool(int(v))), int)
 
 
 def scpi_num(dtype):
     """Get a SCPI converter for unit-less numbers.
 
     :param dtype: name of type to convert to (e.g. 'int', 'float')
-    :return: ScpiTypeConverter for nums
+    :return: ValueConverter for nums
     """
-    return ScpiTypeConverter(getattr(builtins, dtype),
-                             lambda v: v)
+    return ValueConverter(getattr(builtins, dtype), lambda v: v)
 
 
 @lru_cache()  # don't create multiple functions for the same conversion
@@ -97,14 +92,14 @@ def scpi_qty(unit_str):
     useful.
 
     :param unit_str: string representation of the unit for this command
-    :return: ScpiTypeConverter for quantities
+    :return: ValueConverter for quantities
     """
     if unit_str == '%':
-        converter = ScpiTypeConverter(lambda v: float(v) / 100,
-                                      lambda v: v * 100)
+        converter = ValueConverter(lambda v: float(v) / 100,
+                                   lambda v: v * 100)
     else:
-        converter = ScpiTypeConverter(lambda v: float(v) * ureg(unit_str),
-                                      qty_write_converter(unit_str))
+        converter = ValueConverter(lambda v: float(v) * ureg(unit_str),
+                                   qty_write_converter(unit_str))
     return converter
 
 
@@ -117,150 +112,46 @@ def scpi_choice(choices):
     names for the keys and instrument names for the values.
 
     :param choices: list or dict of choice options
-    :return: ScpiTypeConverter for a list of choices
+    :return: ValueConverter for a list of choices
     """
     try:
         inv_choices = {v: k for k, v in choices.items()}
     except AttributeError:
         inv_choices = choices
 
-    return ScpiTypeConverter(lambda v: inv_choices[v], lambda v: choices[v])
+    return ValueConverter(lambda v: inv_choices[v], lambda v: choices[v])
 
 
 # converters for strings (needed?), binary (e.g. curve)?
 
 
-def scpi_subsystems_from_specs(channel_specs):
-    sub_systems = {}
-    for channel_id, json_file in channel_specs.items():
-        with open(json_file, 'r') as fp:
-            js = json.load(fp)
-        if isinstance(channel_id, tuple):
-            scpi_sub = scpi_subsystem_from_json(js, channel_id[0])
-        else:
-            scpi_sub = scpi_subsystem_from_json(js)
-        sub_systems[channel_id] = scpi_sub
-    return sub_systems
+scpi_cmd_map = {'source.enabled': 'source{source}',
+                'source.shape': 'source{source}:function:shape',
+                'source.frequency': 'source{source}:frequency',
+                'source.amplitude': 'source{source}:voltage',
+                'source.offset': 'source{source}:voltage:offset',
+                'source.amplitude_unit': 'source{source}:voltage:unit'
+                }
 
 
-def scpi_subsystem_from_json(json_str, *args):
-    """Build or add on to a SCPI Subsystem class from a json description.
+class ScpiFactory:
+    @staticmethod
+    def get_converters(converter_type, *args):
+        converters = {'error': scpi_error,
+                      'bool': scpi_bool,
+                      'qty': scpi_qty,
+                      'choice': scpi_choice}
+        return converters[converter_type](*args)
 
-    :param json_str: json string describing the SCPI subsystem
-    :param base_cls: class to add commands to. If `None`, a new class is made.
-    :param args: used as formatters for `scpi_base_cmd`s
-    :return: updated class object
-    """
-    meta = json_str[0]
-    base_cls = get_blank_scpi_subsystem(meta['name'], meta['description'])
+    @staticmethod
+    def query_func(name, converter, keyword=None):
+        cmd = _build_command(scpi_cmd_map[name], keyword)
+        return _query_func(cmd, converter)
 
-    for cmd in meta['commands']:
-        name, scpi_base_cmd, scpi_type, scpi_type_arg, scpi_kwargs = cmd
-        scpi_base_cmd = scpi_base_cmd.format(*args)
-        scpi_type = globals()[f'scpi_{scpi_type}'](scpi_type_arg)
-        add_scpi_cmd(base_cls, name, scpi_base_cmd, scpi_type, **scpi_kwargs)
-
-    return base_cls
-
-
-def partial_format(st, *args):
-    """Might use this to partially format base cmds"""
-    try:
-        return st.format(*args)
-    except IndexError:
-        return st
-    except KeyError:
-        for arg in args:
-            st = st.replace(f'{{}}', str(arg), 1)
-        return st
-
-
-def add_scpi_cmd(base_cls, name, scpi_base_cmd, converter, *,
-                 can_query=True, can_write=True,
-                 query_keywords=None, write_keywords=None):
-    """Add parameters to a class for SCPI commands.
-
-    Add a property named `name` to the class that calls the corresponding
-    SCPI command. Values are converted as appropriate to translate from
-    Python to the device:
-
-        c.name -> query_converter(query('scpi_base_cmd?'))
-        c.name = value -> write('scpi_base_cmd write(converter(value)')
-
-    If keywords are included, additional properties are added:
-        c.name_qkeyword -> query_converter(query('scpi_base_cmd? qkeyword'))
-        c.name_to_wkeyword -> write('scpi_base_cmd wkeyword')
-
-    This allows for commands that have e.g., min, max, or default values.
-
-    The different converters allow for more customization, including boolean
-    conversions, enforcing units on quantities, and setting a list of choices.
-
-    :param base_cls: class to add on to
-    :param name: name of the attribute or method
-    :param scpi_base_cmd: string for the SCPI command
-    :param converter: ScpiTypeConverter to translate between python and device
-    :param can_query: if True, a query property is added
-    :param can_write: if True, a write property is added
-    :param query_keywords: additional SCPI query keywords for this command
-    :param write_keywords: additional SCPI write keywords for this command
-    """
-    if converter is None:
-        converter = ScpiTypeConverter(None, None)
-
-    # set the property or function
-    if '{' in scpi_base_cmd or not any(converter):
-        # write a function if unfilled {} in base_cmd or no converters
-        if can_query and converter.query is not None:
-            setattr(base_cls, 'get_' + name,
-                    _query_func(scpi_base_cmd, converter.query))
-        if can_write:
-            w_prefix = 'set_' if converter.write is not None else ''
-            setattr(base_cls, w_prefix + name,
-                    _write_func(scpi_base_cmd, converter.write))
-    else:
-        # write a property
-        prop_get, prop_set = None, None
-        if can_query and converter.query is not None:
-            cmd = _build_command(scpi_base_cmd)
-            prop_get = _query_func(cmd, converter.query)
-        if can_write and converter.write is not None:
-            cmd = _build_command(scpi_base_cmd, is_query=False)
-            prop_set = _write_func(cmd, converter.write)
-
-        setattr(base_cls, name, property(prop_get, prop_set))
-
-    # set additional properties for the keywords
-    if query_keywords is None:
-        query_keywords = []
-    for keyword in query_keywords:
-        cmd = _build_command(scpi_base_cmd, keyword)
-        setattr(base_cls, f'{name}_{keyword}',
-                property(_query_func(cmd, converter.query)))
-
-    if write_keywords is None:
-        write_keywords = []
-    for keyword in write_keywords:
-        cmd = _build_command(scpi_base_cmd, keyword, is_query=False)
-        setattr(base_cls, f'{name}_to_{keyword}', _write_func(cmd, None))
-
-
-def get_blank_scpi_subsystem(cls_name, cls_description=None):
-    """Create a new, blank class to build upon.
-
-    :param cls_name: name of the class
-    :param cls_description: docstring description
-    :return: class
-    """
-
-    def __init__(self, parent):
-        self._parent = weakref.ref(parent)
-        self.keys = {}
-
-    base_cls = type(cls_name, (object,), {})
-    base_cls.__init__ = __init__
-    base_cls.__doc__ = cls_description
-    return base_cls
+    @staticmethod
+    def write_func(name, converter, keyword=None):
+        cmd = _build_command(scpi_cmd_map[name], keyword, is_query=False)
+        return _write_func(cmd, converter)
 
 
 def _build_command(base_cmd, post=None, *, is_query=True):
@@ -272,14 +163,14 @@ def _build_command(base_cmd, post=None, *, is_query=True):
     :return: string command
     """
     q = '?' if is_query else ''
-    command = base_cmd + q
-
-    if post:
-        command += ' ' + post
+    if post is not None:
+        post = ' ' + post
     elif not is_query:
-        command += ' {value}'
+        post = ' {value}'
+    else:
+        post = ''
 
-    return command
+    return f'{base_cmd}{q}{post}'
 
 
 def _query_func(command, converter):
@@ -290,12 +181,9 @@ def _query_func(command, converter):
     :return: SCPI query function
     """
 
-    if '{' in command:
-        def query_func(self, *args):
-            return converter(self._parent().device.query(command.format(*args)))
-    else:
-        def query_func(self):
-            return converter(self._parent().device.query(command))
+    def query_func(self):
+        return converter(
+            self._parent().device.query(command.format(**self.keys)))
 
     return query_func
 
---
+++ patchbay/hardware/subsystem.py
@@ -0,0 +1,129 @@
+import json
+import weakref
+from collections import namedtuple
+from os import path
+
+definition_file = path.join(path.dirname(__file__),
+                            'subsystem_definitions.json')
+with open(definition_file, 'r') as fp:
+    prototype_definitions = json.load(fp)
+
+ValueConverter = namedtuple('ValueConverter', 'query, write')
+
+
+def subsystem_factory(prototype, cmd_factory):
+    """Build a subsystem from the prototype using the given command factory.
+
+    Prototype is a dictionary with the keys: name, description, iterable,
+    commands, subsystems.
+
+    Each command definition is an iterable with elements: name,
+    converter_type, converter_arg, kwargs
+
+    todo: full writeup of the format once this has been refined.
+
+    :param prototype: definition of the subsystem commands
+    :param cmd_factory: factory for generating the functions for a device
+    :return: class
+    """
+    base_cls = get_new_subsystem(prototype['name'], prototype['description'])
+
+    for cmd in prototype['commands']:
+        name, cmd_type, cmd_arg, kwargs = cmd
+        add_cmd(base_cls, cmd_factory, name, cmd_type, cmd_arg, **kwargs)
+
+    return base_cls
+
+
+def add_cmd(base_cls, factory, name, converter_type, converter_arg=None, *,
+            can_query=True, can_write=True,
+            query_keywords=None, write_keywords=None):
+    """Add command parameters and methods to a subsystem class.
+
+    Add a properties and methods associated with `name` to the subsystem
+    class. Values are converted as appropriate to translate from Python to
+    the device:
+
+        c.name -> query_converter(query_cmd())
+        c.name = value -> write_cmd(write_converter(value))
+
+    If converters are None, then a command without parameters is added: name()
+
+    If keywords are included, additional properties are added for each:
+    `c.name_qkeyword()` and `c.name_to_wkeyword()`
+
+    This allows for commands that have e.g., min, max, or default values.
+
+    The different converters allow for more customization, including boolean
+    conversions, enforcing units on quantities, and setting a list of choices.
+
+    :param base_cls: class to add on to
+    :param factory: class for generating the appropriate functions
+    :param name: name of the attribute or method
+    :param converter_type: ValueConverter specific for device and argument
+    :param converter_arg: argument passed to converter constructor, if any
+    :param can_query: if True, a query property is added
+    :param can_write: if True, a write property is added
+    :param query_keywords: additional SCPI query keywords for this command
+    :param write_keywords: additional SCPI write keywords for this command
+    """
+    if query_keywords:
+        query_keywords = [key.strip() for key in query_keywords.split(',')]
+    if write_keywords:
+        write_keywords = [key.strip() for key in write_keywords.split(',')]
+    if converter_type == 'choice':
+        converter_arg = [arg.strip() for arg in converter_arg.split(',')]
+
+    converter = factory.get_converters(converter_type, converter_arg)
+    fullname = f'{base_cls.__name__.lower()}.{name}'
+
+    # set the property or function
+    if not any(converter):
+        # write a function if no converters
+        if can_query and converter.query is not None:
+            setattr(base_cls, 'get_' + name,
+                    factory.query_func(fullname, converter.query))
+        if can_write:
+            w_prefix = 'set_' if converter.write is not None else ''
+            setattr(base_cls, w_prefix + name,
+                    factory.write_func(fullname, converter.write))
+    else:
+        # write a property
+        prop_get, prop_set = None, None
+        if can_query and converter.query is not None:
+            prop_get = factory.query_func(fullname, converter.query)
+        if can_write and converter.write is not None:
+            prop_set = factory.write_func(fullname, converter.write)
+
+        setattr(base_cls, name, property(prop_get, prop_set))
+
+    # set additional properties for the keywords
+    if query_keywords is None:
+        query_keywords = []
+    for key in query_keywords:
+        setattr(base_cls, f'{name}_{key}',
+                property(factory.query_func(fullname, converter.query, key)))
+
+    if write_keywords is None:
+        write_keywords = []
+    for key in write_keywords:
+        setattr(base_cls, f'{name}_to_{key}',
+                factory.write_func(fullname, None, key))
+
+
+def get_new_subsystem(name, description=None):
+    """Create a new, blank class to build upon.
+
+    :param name: name of the class
+    :param description: docstring description
+    :return: class
+    """
+
+    def __init__(self, parent):
+        self._parent = weakref.ref(parent)
+        self.keys = {}
+
+    base_class = type(name.capitalize(), (object,), {})
+    base_class.__init__ = __init__
+    base_class.__doc__ = description
+    return base_class
