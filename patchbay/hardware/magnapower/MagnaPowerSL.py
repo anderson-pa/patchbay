9a820c2938a6d22fae55db86a1b5728933235d12
Phillip Anderson <anderson.pa@gmail.com>
2020-12-15 08:39:04 -0500

    Add commands for AgilentN3300 and MagnaPowerSL.

--- patchbay/hardware/magnapower/MagnaPowerSL.py
+++ patchbay/hardware/magnapower/MagnaPowerSL.py
@@ -1,7 +1,4 @@
 from patchbay.hardware import scpi
-from patchbay.hardware.subsystem import CmdDef
-
-prototype_definitions = {}
 
 
 class MagnaPowerSLSupply(scpi.ScpiNode):
@@ -9,10 +6,10 @@ class MagnaPowerSLSupply(scpi.ScpiNode):
     def __init__(self, device):
         super().__init__(device)
 
-        self.source = None
         scpi.ScpiFactory.add_subsystem(self, 'measure', measure_cmds)
         scpi.ScpiFactory.add_subsystem(self, 'output', output_cmds)
         scpi.ScpiFactory.add_subsystem(self, 'source', source_cmds)
+        scpi.ScpiFactory.add_subsystem(self, 'control', control_cmds)
 
     def _get_serial(self, idn_part):
         # device returns field as 'S/N:xxxx-yyyy', cut off prefix including colon
@@ -25,21 +22,28 @@ class MagnaPowerSLSupply(scpi.ScpiNode):
         return versions
 
 
-measure_cmds = [CmdDef('voltage', 'voltage', 'qty', 'V', {'can_write': False}),
-                CmdDef('current', 'current', 'qty', 'A', {'can_write': False}),
+measure_cmds = [('voltage', 'voltage', 'qty', 'V', {'can_write': False}),
+                ('current', 'current', 'qty', 'A', {'can_write': False}),
                 ]
 
-output_cmds = [('enabled', 'state', 'bool', None, {'can_write':False}),
+output_cmds = [('enabled', 'state', 'bool', None, {'can_write': False}),
                ('enabled', ('start', 'stop'), 'bool', None,
                 {'can_query': False, 'split_cmd': True})
                ]
+
+control_cmds = [('external', 'external', 'bool', None),
+                ('internal', 'internal', 'bool', None),]
+
 
 min_max_keywords = {f'{key}_keywords': ['min', 'max']
                     for key in ['query',]}
 
 source_cmds = [('voltage', 'volt', 'qty', 'V', min_max_keywords),
                ('triggered_voltage', 'volt:trig', 'qty', 'V', min_max_keywords),
+               ('voltage_protection', 'volt:prot', 'qty', 'V', min_max_keywords),
                ('current', 'curr', 'qty', 'A', min_max_keywords),
                ('triggered_current', 'curr:trig', 'qty', 'A', min_max_keywords),
+               ('current_protection', 'curr:prot', 'qty', 'A', min_max_keywords),
                ]
+
 
